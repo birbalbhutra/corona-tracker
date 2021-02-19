4cdc490ba1393dce1e279bca3d3804d9d6381969
birbalbhutra <bhutrabirbal@gmail.com>
2021-02-19 12:55:33 +0530

    added api, cards and chart

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { fetchDailyData } from "../../api";
+import { Line, Bar } from "react-chartjs-2";
+
+import styles from "./Chart.module.css";
 
 const Chart = () => {
-    return(
-        <h1>Chart</h1>
-    )
-}
+  const [dailyData, setDailyData] = useState({});
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setDailyData(await fetchDailyData());
+    };
+    console.log(dailyData);
+    fetchAPI();
+  }, []);
+
+  const lineChart = dailyData[0] ? (
+    <Line
+      data={{
+        labels: dailyData(({ date }) => date),
+        datasets: [
+          {
+            data: dailyData(({ confirmed }) => confirmed),
+            label: "Infected",
+            borderColor: "#3333ff",
+            fill: true,
+          },
+          {},
+        ],
+      }}
+    />
+  ) : null;
+
+  return <div>Chart</div>;
+};
 
 export default Chart;
